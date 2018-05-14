7c74e3fee721e6a01380ac5fe58f8db706bb0d8b
Hassan Ali <helfi92@gmail.com>
2018-05-14 08:39:19 -0400

    Add the route /auth/scopes and create Scopes and Scope view
    Also a small nit to the Scope view

---
+++ src/views/Scopes/index.jsx
@@ -0,0 +1,37 @@
+import { hot } from 'react-hot-loader';
+import { Component } from 'react';
+import { Switch } from 'react-router-dom';
+import RouteWithProps from '../../components/RouteWithProps';
+import loadable from '../../utils/loadable';
+
+const ViewScopes = loadable(() =>
+  import(/* webpackChunkName: 'Clients.ViewScopes' */ './ViewScopes')
+);
+const ViewScope = loadable(() =>
+  import(/* webpackChunkName: 'Clients.ViewScope' */ './ViewScope')
+);
+
+@hot(module)
+export default class Scopes extends Component {
+  render() {
+    const {
+      match: { path },
+      ...props
+    } = this.props;
+
+    return (
+      <Switch>
+        <RouteWithProps
+          path={`${path}/:view(clients|roles)/:selectedScope/:selectedScopeView(clients|roles)?`}
+          {...props}
+          component={ViewScope}
+        />
+        <RouteWithProps
+          path={`${path}/:view(clients|roles)?`}
+          {...props}
+          component={ViewScopes}
+        />
+      </Switch>
+    );
+  }
+}
