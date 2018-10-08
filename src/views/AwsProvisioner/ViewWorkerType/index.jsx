66a52d37c961070c2cf274cd0d47322e169adc94
M Christensen <michael.christensen@wsu.edu>
2018-10-07 20:39:26 -0700

    Removing Terminate all while working on adding Snackbar
    During the implemenation of the implementation of Snackbar, I wanted to try to see if I could change the buttons at the AWS section of the website and came up with this solutions.

--- src/views/AwsProvisioner/ViewWorkerType/index.jsx
+++ src/views/AwsProvisioner/ViewWorkerType/index.jsx
@@ -148,6 +148,7 @@ export default class ViewWorkerType extends Component {
           )}
         {!error &&
           !loading &&
+          awsProvisionerWorkerTypeState.instances !== 0 &&
           currentTab === 3 && (
             <Tooltip title="Terminate All">
               <div className={classes.fab}>
