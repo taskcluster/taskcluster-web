35d4ff257d4ee46d457cd6b271e68fc9e6c6c6a4
Sam Young <youngsam@oregonstate.edu>
2018-10-26 12:20:42 -0700

    Added button outside of menu

--- src/views/Tasks/CreateTask/index.jsx
+++ src/views/Tasks/CreateTask/index.jsx
@@ -220,7 +220,10 @@ export default class CreateTask extends Component {
       createdTaskId,
       loading,
     } = this.state;
-
+    const buttonstyle = {
+      paddingRight: '200px',
+      cursor: 'pointer',
+    }
     if (createdTaskId && interactive) {
       return <Redirect to={`/tasks/${createdTaskId}/connect`} push />;
     }
@@ -277,6 +280,20 @@ export default class CreateTask extends Component {
                 value={task || ''}
                 onChange={this.handleTaskChange}
               />
+
+                    <button
+                      style={buttonstyle}
+                      requiresAuth
+                      tooltipOpen
+                      icon={<PlusIcon />}
+                      onClick={this.handleCreateTask}
+                      tooltipTitle="Create Task"
+                      classes={{ button: classes.createIcon }}
+                      ButtonProps={{
+                        disabled: !task || invalid || loading,
+                      }}
+                    >Add Task</button>
+
               <SpeedDial>
                 <SpeedDialAction
                   requiresAuth
