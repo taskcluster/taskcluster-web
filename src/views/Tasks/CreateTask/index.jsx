e02423851548ec6b30338130126ef02284382582
Sam Young <youngsam@oregonstate.edu>
2018-10-27 11:32:40 -0700

    Fixed for Travis CI

--- src/views/Tasks/CreateTask/index.jsx
+++ src/views/Tasks/CreateTask/index.jsx
@@ -235,7 +235,7 @@ export default class CreateTask extends Component {
       fontSize: 16,
       cursor: 'pointer',
       marginTop: 10,
-    }
+    };
     if (createdTaskId && interactive) {
       return <Redirect to={`/tasks/${createdTaskId}/connect`} push />;
     }
