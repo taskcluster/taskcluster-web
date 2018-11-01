56099494d29f553d3288609c8236228bb940eecb
Sam Young <youngsam@oregonstate.edu>
2018-10-31 21:50:05 -0700

    Fixed button color error by putting the styling in the buttonIcon css

--- src/views/Tasks/CreateTask/index.jsx
+++ src/views/Tasks/CreateTask/index.jsx
@@ -62,15 +62,17 @@ const defaultTask = {
 @withStyles(theme => ({
   createIcon: {
     ...theme.mixins.successIcon,
+<<<<<<< HEAD
     position: 'fixed',
     bottom: theme.spacing.double,
     right: theme.spacing.unit * 11,
   },
   toolTip: {
+=======
+>>>>>>> Fixed button color error by putting the styling in the buttonIcon css
     position: 'fixed',
-    bottom: 16,
-    right: 90,
-    background: '#4CAF50',
+    bottom: theme.spacing.double,
+    right: theme.spacing.unit * 11,
   },
 }))
 export default class CreateTask extends Component {
