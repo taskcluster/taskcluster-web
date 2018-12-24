dca543de8f4ee568790a19ab8faa9a684ade36dc
Arshad Kazmi <arshadkazmi42@gmail.com>
2018-12-25 00:38:59 +0530

    Update withAlertOnClose to Form pages.

--- src/views/Display/index.jsx
+++ src/views/Display/index.jsx
@@ -10,7 +10,9 @@ import Dashboard from '../../components/Dashboard';
 import DataTable from '../../components/DataTable';
 import { VNC_DISPLAYS_POLLING_INTERVAL } from '../../utils/constants';
 import ErrorPanel from '../../components/ErrorPanel';
+import withAlertOnClose from '../../utils/withAlertOnClose';
 
+@withAlertOnClose
 @withStyles({
   vncDisplay: {
     padding: 0,
