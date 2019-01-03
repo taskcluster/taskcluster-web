f8ddfb23334de14d23de5763af41bc8f117a0fba
Hassan Ali <helfi92@gmail.com>
2019-01-03 15:20:27 -0500

    Let users know when auth is required for disabled buttons

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 import MuiButton from '@material-ui/core/Button';
 import Fab from '@material-ui/core/Fab';
-import { node, bool, oneOf } from 'prop-types';
+import Tooltip from '@material-ui/core/Tooltip';
+import { string, object, node, bool, oneOf } from 'prop-types';
 import { withAuth } from '../../utils/Auth';
 import { gaEvent } from '../../utils/prop-types';
 
@@ -20,6 +21,8 @@ export default class Button extends Component {
     requiresAuth: false,
     track: null,
     variant: null,
+    tooltip: null,
+    spanProps: null,
   };
 
   static propTypes = {
@@ -33,6 +36,19 @@ export default class Button extends Component {
     track: gaEvent,
     /** The variant to use. */
     variant: oneOf(['text', 'outlined', 'contained', 'round', 'extended']),
+    /** Properties applied to the Tooltip component */
+    tooltipProps: object,
+    /** Label to display in the tooltip. */
+    tooltipTitle: string,
+    /**
+     * Properties applied to the span wrapper element
+     * This is only applicable when there `tooltipProps` is set.
+     * This is useful for positioning the button.
+     *
+     * Note: We wrap a button with a span in order to
+     * be able to show tooltips on disabled buttons.
+     * */
+    spanProps: object,
   };
 
   handleButtonClick = () => {
@@ -60,13 +76,15 @@ export default class Button extends Component {
       track,
       onAuthorize,
       onUnauthorize,
+      tooltipProps,
+      spanProps,
       ...props
     } = this.props;
-    const isDisabled = (requiresAuth && !user) || disabled;
+    const lackingAuth = requiresAuth && !user;
+    const isDisabled = lackingAuth || disabled;
     const MuiComponent =
       variant === 'round' || variant === 'extended' ? Fab : MuiButton;
-
-    return (
+    const ButtonComponent = (
       <MuiComponent
         onClick={this.handleButtonClick}
         disabled={isDisabled}
@@ -74,6 +92,17 @@ export default class Button extends Component {
         {...props}>
         {children}
       </MuiComponent>
+    );
+    const tooltipTitle = tooltipProps && tooltipProps.title;
+
+    return tooltipProps ? (
+      <Tooltip
+        {...tooltipProps}
+        title={lackingAuth ? `${tooltipTitle} (Auth Required)` : tooltipTitle}>
+        <span {...spanProps}>{ButtonComponent}</span>
+      </Tooltip>
+    ) : (
+      ButtonComponent
     );
   }
 }
